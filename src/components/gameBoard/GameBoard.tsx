35c8040f86610e6aed8e55ca6b541577bef7e47f
한상준 <hsjun1996@naver.com>
2023-05-24 13:04:20 +0900

    FIX : png->wepb, backgrounImage inline에서 external

--- src/components/gameBoard/GameBoard.tsx
+++ src/components/gameBoard/GameBoard.tsx
@@ -1,13 +1,9 @@
 import styles from "./GameBoard.module.css";
-import GameBoardImage from "../../assets/images/image-game-board.png";
 import AvatarImage from "./AvatarImage";
 
 const GameBoard = () => {
   return (
-    <div
-      className={styles.game_table}
-      style={{ backgroundImage: `url(${GameBoardImage})` }}
-    >
+    <div className={styles.game_table}>
       <div className={styles.game_table_settings}></div>
       <div className={styles.game_settings}>
         <AvatarImage />
