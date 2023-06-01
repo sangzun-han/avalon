aa0476ec3ec029a59dfee639190a68825ec47acf
임경찬 <rudcks5562@naver.com>
2023-06-02 01:12:17 +0900

    add : basic_demo added

---
+++ backend/src/main/java/com/ssafy/demo/entity/GameLog.java
@@ -0,0 +1,14 @@
+//package com.ssafy.demo.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//public class GameLog {
+//}
---
+++ backend/src/main/java/com/ssafy/demo/entity/PlayRoom.java
@@ -0,0 +1,33 @@
+//package com.ssafy.demo.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//import java.util.Date;
+//
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//public class PlayRoom {
+//
+//    @Id
+//    long room_id;
+//    @OneToOne
+//    @JoinColumn(name = "nickname")
+//    @Column(nullable = false)
+//    String host_nickname;
+//
+//
+//    @Column(nullable = false)
+//    LocalDateTime create_time;
+//
+//
+//
+//
+//
+//
+//}
