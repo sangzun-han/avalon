a76eade3e06b59db0df2157227c1fbc930620883
한상준 <hansangjun@hansangjun-ui-MacBookAir.local>
2023-06-26 17:16:29 +0900

    Design : Lobby

--- frontend/src/components/lobby/Lobby.tsx
+++ frontend/src/components/lobby/Lobby.tsx
@@ -1,41 +1,45 @@
 import styles from "./Lobby.module.css";
-import OnlineUserList from "./OnLineUserList";
 
 const Lobby = () => {
-  const onlineUsers = ["User1", "User2", "User3", "User4"];
+  // const onlineUsers = ["User1", "User2", "User3", "User4"];
   return (
     <main className={styles.layout}>
-      <OnlineUserList onlineUsers={onlineUsers} />
-      {/* <section className="game-list">
-        <h2>게임 목록</h2>
-        <ul>
-          <li>
-            <img src="game1.png" alt="게임 1" />
-            <h3>게임 1</h3>
-            <p>게임 1의 설명</p>
-            <button>플레이</button>
-          </li>
-          <li>
-            <img src="game2.png" alt="게임 2" />
-            <h3>게임 2</h3>
-            <p>게임 2의 설명</p>
-            <button>플레이</button>
-          </li>
-        </ul>
-      </section>
+      <section className={styles.lobby}>
+        <header>
+          <h1 className={styles.title}>AVALON</h1>
+          <button className={styles.danger}>로그아웃</button>
+        </header>
+        <section className={styles.room_button}>
+          <button className={styles.btn}>방만들기</button>
+        </section>
 
-      <section className="chat">
-        <h2>채팅</h2>
-        <div id="chat-messages"></div>
-        <form id="chat-form">
-          <input
-            type="text"
-            id="message-input"
-            placeholder="메시지를 입력하세요"
-          />
-          <button type="submit">전송</button>
-        </form>
-      </section> */}
+        <section className={styles.game_rooms}>
+          <div className={styles.room}>
+            <h3>Room 1</h3>
+            <p>Some description about Room 1.</p>
+          </div>
+          <div className={styles.room}>
+            <h3>Room 1</h3>
+            <p>Some description about Room 1.</p>
+          </div>
+          <div className={styles.room}>
+            <h3>Room 1</h3>
+            <p>Some description about Room 1.</p>
+          </div>
+          <div className={styles.room}>
+            <h3>Room 1</h3>
+            <p>Some description about Room 1.</p>
+          </div>
+          <div className={styles.room}>
+            <h3>Room 1</h3>
+            <p>Some description about Room 1.</p>
+          </div>
+          <div className={styles.room}>
+            <h3>Room 1</h3>
+            <p>Some description about Room 1.</p>
+          </div>
+        </section>
+      </section>
     </main>
   );
 };
